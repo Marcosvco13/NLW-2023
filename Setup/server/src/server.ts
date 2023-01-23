9a20bcbdf157fc9d1fbbf3b29c5f0012d392e8c7
Marcos Vinícius <84818139+Marcosvco13@users.noreply.github.com>
2023-01-23 20:55:30 -0300

    1.1

--- Setup/server/src/server.ts
+++ Setup/server/src/server.ts
@@ -1,11 +1,19 @@
 import fastify from "fastify";
+import cors from "@fastify/cors"
+import {PrismaClient} from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient
 
-app.get('/', () => {
-    return 'Hello World'
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
 })
 
 app.listen({
     port: 3333,
+}).then(() => {
+    console.log('HTTP Server Running!')
 })
